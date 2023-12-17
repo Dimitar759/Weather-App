93c5b160c02b0e6830ef4ef47ff4423727b519ce
Dimitar759 <142807026+Dimitar759@users.noreply.github.com>
2023-12-17 20:47:50 +0100

    Weather app project
    html API Css and some javascript added to the project

---
+++ script.js
@@ -0,0 +1,23 @@
+const apiKey = "e2a608f92d298545c46b8f6f8397ec78";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
+
